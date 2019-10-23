7c2482afd18e43dd064463f50327074d161747f9
Kelvin Brito <britok30@gmail.com>
2019-10-23 17:53:23 -0400

    first commit

---
+++ src/components/NavOne/NavOne.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Navbar} from "react-bootstrap"
+
+const NavOne = () => {
+    return (
+        <div>
+            Apple
+        </div>
+    )
+}
+
+export default NavOne
