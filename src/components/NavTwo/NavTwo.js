93916f4d7d2d9c23104d159ff56371f9058abd4b
Kelvin Brito <britok30@gmail.com>
2019-10-24 03:45:20 -0400

    fix

--- src/components/NavTwo/NavTwo.js
+++ src/components/NavTwo/NavTwo.js
@@ -9,23 +9,37 @@ const NavTwo = () => {
     <Navbar className="navtwo" expand="lg">
       <Navbar.Brand className="brand2" href="#home">
         <Link to="/main">
-          <img className="logo2 wow fadeInUp" data-wow-delay=".7s" src={logoblack} alt="" />
+          <img
+            className="logo2 wow fadeInUp"
+            data-wow-delay=".7s"
+            src={logoblack}
+            alt=""
+          />
         </Link>
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="basic-navbar-nav" />
       <Navbar.Collapse id="basic-navbar-nav">
         <Nav className="ml-auto">
-          <Nav.Link className="navlink wow fadeInUp" data-wow-delay=".7s" href="/iphone" active>
-            iPhone
+          <Nav.Link
+            className="navlink wow fadeInUp"
+            data-wow-delay=".7s"
+            active
+          >
+            <Link to="/iphone">iPhone</Link>
           </Nav.Link>
-          <Nav.Link className="navlink wow fadeInUp" data-wow-delay=".9s" href="/macbook">
-            MacBook Pro
+          <Nav.Link className="navlink wow fadeInUp" data-wow-delay=".9s">
+            <Link to="/macbook">MacBook</Link>
           </Nav.Link>
-          <Nav.Link className="navlink wow fadeInUp" data-wow-delay="1.1s" href="/watch">
-            Watch
+          <Nav.Link className="navlink wow fadeInUp" data-wow-delay="1.1s">
+            <Link to="/watch">Watch</Link>
           </Nav.Link>
           <Nav.Link href="#">
-            <button className="btn btn-primary btn-prime wow fadeInUp" data-wow-delay="1.3s">Notify Me</button>
+            <button
+              className="btn btn-primary btn-prime wow fadeInUp"
+              data-wow-delay="1.3s"
+            >
+              Notify Me
+            </button>
           </Nav.Link>
         </Nav>
       </Navbar.Collapse>
