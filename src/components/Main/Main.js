754000f694bf60192ea704c242ac9666bebb9882
Kelvin Brito <britok30@gmail.com>
2020-04-27 23:57:10 -0400

    hero changes

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -7,19 +7,6 @@ const Main = () => {
         <Fragment>
             <div className="mainbody">
                 <div className="container hero">
-                    <div className="main-text">
-                        <p
-                            className="welcome wow fadeInUp"
-                            data-wow-delay=".7s"
-                        >
-                            Welcome to Apple
-                        </p>
-                        <Link to="/iphone" className="product-link">
-                            <p className="wow fadeInUp" data-wow-delay=".8s">
-                                See our new products
-                            </p>
-                        </Link>
-                    </div>
                     <Icon />
                 </div>
                 <div className="container">
